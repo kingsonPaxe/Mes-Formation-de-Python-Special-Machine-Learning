da88c808123d5f1efb776c13937f70593c8258ee
KingsonPaxe <jeovanijp972@gmai.com>
2025-02-16 07:55:19 +0100

    En ajoutant nouveaux projects

--- leçon15 - Graphiques importantes!/index.py
+++ leçon15 - Graphiques importantes!/index.py
@@ -22,24 +22,39 @@ import numpy as np
 # plt.show()
 
 
-x = np.linspace(0,5,100)
-y = np.linspace(0,5,100)
-print(f"x_menor = {x}\n")
-print(f"y_menor = {y}\n")
+# x = np.linspace(0,5,100)
+# y = np.linspace(0,5,100)
+# print(f"x_menor = {x}\n")
+# print(f"y_menor = {y}\n")
 
-X, Y= np.meshgrid(x,y)
-print(f"X = {X}\n e tem um tamanho de {X.shape}")
-print(f"Y = {Y}\n e tem um tamanho de {X.shape}")
+# X, Y= np.meshgrid(x,y)
+# print(f"X = {X}\n e tem um tamanho de {X.shape}")
+# print(f"Y = {Y}\n e tem um tamanho de {X.shape}")
 
-f = lambda x, y: np.sin(x) + np.cos(x+y)
-z = f(X,Y)
+# f = lambda x, y: np.sin(x) + np.cos(x+y)
+# z = f(X,Y)
 
-print(f"Z = {z}\n")
-axes = plt.axes(projection = "3d")
-axes.plot_surface(X,Y,z, cmap = "plasma")
-plt.title("En utilisant la function meshgrid ")
-plt.contour(X,Y,z)
-plt.colorbar()
-plt.xlabel("x")
-plt.ylabel("y")
+# print(f"Z = {z}\n")
+# axes = plt.axes(projection = "3d")
+# axes.plot_surface(X,Y,z, cmap = "plasma")
+# plt.title("En utilisant la function meshgrid ")
+# plt.contour(X,Y,z)
+# plt.colorbar()
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.show()
+
+from sklearn.datasets import load_iris
+iris = load_iris()
+x = iris.data # Onde esta os dados
+y = iris.target # Classe ou tipo de flores
+names = list(iris.target_names)
+
+ax = plt.axes(projection='3d')
+ax.scatter(x[:,0], x[:,1], x[:,2], c = y)
+
+plt.ylabel('y')
+plt.xlabel('x')
+handle = [plt.Line2D([0],[0], marker = "o", label = name, markerfacecolor=plt.cm.viridis((i/2)), color="w", markersize=8) for i, name in enumerate(names)]
+plt.legend(handles = handle)
 plt.show()
---
+++ leçon15 - Graphiques importantes!/interpolacao.py
@@ -0,0 +1,9 @@
+# Decloarar uma variavel do tipo int 
+# Atribuir um valorr para esta variavel
+# Imprima o valor desta variavel da seguinte maneira:
+
+# "O valor atribuido a variavel foi:_____ "
+
+val = int(input("Digite um numero qualquer: "))
+
+print(f"O valor atribuido a variavel foi: {val}")
